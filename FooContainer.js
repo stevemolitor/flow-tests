ffe1a1190389d13de0c3da7c78ccdec11c34e00a
Vitor Balocco <vitorbal@gmail.com>
2018-02-28 14:21:17 +0100

    Working example with lodash flowRight and an extra HOC
    This shows that flow can properly typecheck an incorrect Prop
    flowing through a `_.flowRight` call and two HOCs: `connect()`
    and a custom `addID` HOC.

--- FooContainer.js
+++ FooContainer.js
@@ -1,15 +1,24 @@
 // @flow
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
-type Props = { name: string };
-const Foo = (props: Props) => <div className={props.name} />;
+type Props = {
+  name: string,
+  id: number,
+};
+const Foo = (props: Props) => <div className={props.name} key={props.id} />;
 
 function mapStateToProps() {
   return {
-    name: true,
+    name: 'Vitor',
   };
 }
-const FooContainer = connect(mapStateToProps, null)(Foo);
+
+function addID(Component) {
+  return props => <Component {...props} id={'not-an-id'} />;
+}
+
+const FooContainer = _.flowRight(addID, connect(mapStateToProps, null))(Foo);
 
 export default FooContainer;
